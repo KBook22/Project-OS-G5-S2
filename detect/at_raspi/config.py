6b202582d71da8a0706a9416ac216c2094be1016
Book22RPi5SDCARD <kittisakbook.22@gmail.com>
2026-01-06 16:09:55 +0700

    use apsolute path for config & add new model(trash)

--- detect/at_raspi/config.py
+++ detect/at_raspi/config.py
@@ -1,6 +1,6 @@
 # config.py
 
-MODEL_PATH = "../seperate-v8s.pt"
+MODEL_PATH = "/home/f11man/project-os/Project-OS-G5-S2/detect/seperate-v8s.pt"
 
 TESSERACT_CHAR_CONFIG = "--psm 6 -l tha"
 TESSERACT_PROVINCE_CONFIG = "--psm 6 -l tha"
